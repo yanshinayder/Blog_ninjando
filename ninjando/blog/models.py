0ab8aa4c46f96986f00366de02de7cc9b26d0d3a
Yan <shinayderepcar@outlook.com>
2021-08-12 22:28:01 -0300

    Código

--- ninjando/blog/models.py
+++ ninjando/blog/models.py
@@ -1,37 +1,19 @@
-from datetime import datetime
-
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE, SET_NULL
-from django.db.models.fields import  SlugField
-from django.urls import reverse
 from django.utils import timezone
-from django.utils.translation import to_language
-from mptt.models import MPTTModel, TreeForeignKey
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-
-from backend.comments.models import AbstractComment 
-from backend.utils.send_mail import send_mail_user_post
 
 
-class Blogcategory(MPTTModel):
+
+class Category(models.Model):
     name = models.CharField("Category", max_length=50)
     published = models.BooleanField("Publish?", default=True)
-    parent = TreeForeignKey (
+    parent = models.ForeignKey(
         'self',
         verbose_name ="Parental Category",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         related_name='children')
-
-    slug = models.SlugField(max_length=100, blank=True, null=True, unique=True)    
-
-    description = models.TextField("Description", max_length=300, default="")
-
-    def  get_absolute_url(self):
-        return reverse("list_category", kwargs={"category": self.slug})
 
     class Meta:
         verbose_name = "Category"    
@@ -40,10 +22,9 @@ class Blogcategory(MPTTModel):
     def __str__(self):
         return self.name
 
-    class Tag(models.Model):
-        name = models.CharField("Tag", max_length=50, unique=True, null=True)
-        slug = models.SlugField(max_length=100, blank=True, null=True)
-
+class Tag(models.Model):
+    name = models.CharField("Tag", max_length=50, unique=True, null=True)
+    
     class Meta:
         verbose_name = "Tag"
         verbose_name_plural = "Tags"
@@ -63,11 +44,58 @@ class Post(models.Model):
     text = models.TextField('full content', max_length=10000000)
     created_date = models.DateTimeField('Creation Date', auto_now_add=True )    
     published_date = models.DateTimeField(
-        "Publication date"
-        default=timezone.now,
+        "Publication date",
+        default= timezone.now,
         blank=True,
-        null=True
-        )        
+        null=True,
+    
     image = models.ImageField("Image"), upload_to="blog/", blank=True)
     tag = models.ManyToManyField(Tag, verbose_name="Ter", blank=True)
-    
+    category = models.ForeignKey(
+        Category,
+        verbose_name = "Category",
+        blank = True,
+        null = True,
+        on_delete=models.SET_NULL
+    )
+    published = models.BooleanField("Publish?", default=True)
+    viewed = models.IntegerField("Visa", default=0)
+
+    class Meta:
+        verbose_name = "News"
+        verbose_name_plural = "News"
+        ordering = ["-created_date"]
+
+    def get_category_description(self):
+        return self.category.description
+
+    def get_count_comments(self):
+        return f"{self.comments.all().count()}"
+
+    def get_comments(self):
+        return self.comments.filter(parent=None)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model ):
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, verbose_name="News", related_name="Comments", on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        "self",
+        verbose_name="Parent Comment",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.post)
